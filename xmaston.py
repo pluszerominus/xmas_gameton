8839ecb9d29c7af18d5009d819cd6e1e2bb22647
pluszerominus <top-bletka@yandex.ru>
2024-12-20 19:31:20 +0300

    Update xmaston.py

--- xmaston.py
+++ xmaston.py
@@ -4,7 +4,7 @@ import json
 
 
 # Ваш токен
-token = 'xxx'
+token = 'a1a34c48-b881-427a-858f-56c5982d68f5'
 # URL сервера
 server_url = 'https://games-test.datsteam.dev/play/snake3d'
 
@@ -25,6 +25,34 @@ headers = {
 
 # Выполнение POST-запроса
 response = requests.post(url, headers=headers, json=data)
+
+map_info = response.json()
+
+# Координаты препядствий
+fence_pos = map_info["fences"]
+
+# Змейки
+snake_1 = map_info["snakes"][0]
+snake_2 = map_info["snakes"][1]
+snake_3 = map_info["snakes"][2]
+
+# Соперники
+enemies = map_info["enemies"]
+
+
+# еда
+food = map_info["food"]
+
+# специальная еда
+special_food = map_info["specialFood"]
+
+# ошибки
+err = map_info["errors"]
+
+print(snake_1)
+print(food)
+
+
 
 # Сохранение ответа в файл
 with open('example_response.json', 'w') as file:
